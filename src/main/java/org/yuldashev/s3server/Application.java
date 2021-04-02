1a92c1ac99253fc021ccd891933de96916ad6379
ChildOfJustice <izzvms@gmail.com>
2021-04-02 14:05:52 +0300

    Moved Application.java.

--- src/main/java/org/yuldashev/s3server/Application.java
+++ src/main/java/org/yuldashev/s3server/Application.java
@@ -1,4 +1,4 @@
-package org.dimyriy.faultyserver;
+package org.yuldashev.s3server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
